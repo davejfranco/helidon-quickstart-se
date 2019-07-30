fb75c61a3e14575ee140d60cfbb4c83ca80b4df7
Dave Franco <davefranco1987@gmail.com>
2019-07-30 10:56:06 -0400

    fix hello testsC

--- src/test/java/io/helidon/examples/quickstart/se/MainTest.java
+++ src/test/java/io/helidon/examples/quickstart/se/MainTest.java
@@ -60,15 +60,15 @@ public class MainTest {
         Assertions.assertEquals(200, conn.getResponseCode(), "HTTP response1");
         JsonReader jsonReader = Json.createReader(conn.getInputStream());
         JsonObject jsonObject = jsonReader.readObject();
-        Assertions.assertEquals("Hello World!!!", jsonObject.getString("message"),
+        Assertions.assertEquals("Hola World!!!", jsonObject.getString("message"),
                 "default message");
 
         conn = getURLConnection("GET", "/greet/Joe");
         Assertions.assertEquals(200, conn.getResponseCode(), "HTTP response2");
         jsonReader = Json.createReader(conn.getInputStream());
         jsonObject = jsonReader.readObject();
-        Assertions.assertEquals("Hello Joe!", jsonObject.getString("message"),
-                "hello Joe message");
+        Assertions.assertEquals("Hola Joe!", jsonObject.getString("message"),
+                "hola Joe message");
 
         conn = getURLConnection("PUT", "/greet/greeting/Hola");
         Assertions.assertEquals(200, conn.getResponseCode(), "HTTP response3");
